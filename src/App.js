9b3010ab2d80f68f65146d6183e33e3876aeddd8
강주희 <kjhee94@naver.com>
2024-07-02 13:16:03 +0900

    router 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    <div>gg</div>
-  );
+  return <>
+    <div>
+        <Header />
+        <Outlet />
+    </div>
+  </>;
 }
 
 export default App;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+    return (
+        <div>
+            에러!에러! 돌아가십시오
+        </div>
+    );
+}
+
